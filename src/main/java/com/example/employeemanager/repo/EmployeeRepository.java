60bbd1ce9d009fd39e77385492f69824109b44bb
AlejandroDelg <a.delgadog.2019@alumnos.urjc.es>
2023-12-20 15:23:38 +0100

    Added UserNotFoundException and more functions in EmployeeService

--- src/main/java/com/example/employeemanager/repo/EmployeeRepository.java
+++ src/main/java/com/example/employeemanager/repo/EmployeeRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-    @Override
-    Optional<Employee> findById(Long aLong);
+
+    public Optional<Employee> findByEmployeeCode(String employeeCode);
 }
--- src/main/java/com/example/employeemanager/service/EmployeeService.java
+++ src/main/java/com/example/employeemanager/service/EmployeeService.java
@@ -2,9 +2,12 @@ package com.example.employeemanager.service;
 
 import com.example.employeemanager.model.Employee;
 import com.example.employeemanager.repo.EmployeeRepository;
+import com.example.employeemanager.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,5 +22,22 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+    public void deleteEmployee(Long id){
+        employeeRepository.deleteById(id);
+    }
+    public Employee findEmployeeById(Long id){
+        return employeeRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User by id " + id + "not found"));
+    }
+    public Optional<Employee> findEmployeeByCode(String code){
+        return employeeRepository.findByEmployeeCode(code);
     }
 }
